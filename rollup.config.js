c24b8f19207b5aedafdc1a9e18bb9cc53b7aa986
刘怡帆 <liuyifan@shiqiao.com>
2022-05-17 16:27:18 +0800

    feat:add rollup defineConfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,36 @@
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/iifeBundle.js',
-      format: 'iife',
-    },
-  ],
-}
+// 使用defineConfig 提供代码提示
+import { defineConfig } from 'rollup'
+
+export default defineConfig([
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/iifeBundle.js',
+        format: 'iife',
+      },
+    ],
+  },
+  {
+    input: 'src/multiple.js',
+    output: [
+      {
+        file: 'dist/cmdBundle.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/esBundle.js',
+        format: 'es',
+      },
+      {
+        file: 'dist/amdBundle.js',
+        format: 'amd',
+      },
+      {
+        file: 'dist/umdBundle.js',
+        name: 'myUmd',
+        format: 'umd',
+      },
+    ],
+  },
+])
